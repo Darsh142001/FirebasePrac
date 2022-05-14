023fa29a33fd8c099bafca2ce9d0b5d7d906d8ff
Darsh142001 <darshikapillay6@gmail.com>
2022-05-14 10:23:45 +0200

    Created the User java class that stores all the user details. I modified the MianActivity so that the user is added to the realtime database in firebase

--- app/src/main/java/za/ac/iie/opsc/firebaseprac/MainActivity.java
+++ app/src/main/java/za/ac/iie/opsc/firebaseprac/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,28 +27,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+    EditText name;
+    EditText surname;
     EditText email;
     EditText password;
+
     Button register;
     TextView goLoginPg;
 
+    //This will Register users to the realtime database in firebase.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
 
-        email = findViewById(R.id.emailET);
+        name = findViewById(R.id.nameET);
+        surname = findViewById(R.id.surnameET);
+        email = (EditText) findViewById(R.id.emailET);
         password = findViewById(R.id.passwordET);
+
         register = findViewById(R.id.registerBtn);
 
         goLoginPg = (TextView) findViewById(R.id.loginPg);
         goLoginPg.setOnClickListener(this);
 
-        setUpUI();
-        setUpListeners();
 
-        //Need to push the realtime_database_connection branch
+
+        //setUpUI();
+        //setUpListeners();
 
     }
 
@@ -65,26 +73,103 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClickRegister(View view)
     {
-        createAccount(email.getText().toString(),password.getText().toString());
+        RegisterUser();
     }
+    /*
+        public void setUpUI()
+        {
 
-    public void setUpUI()
+        }
+
+
+        public void setUpListeners()
+        {
+
+        }
+    */
+    private void RegisterUser()
     {
+        //Converted every value to a string and stored our variables.
+
+        String username = name.getText().toString().trim();
+        String userSurname = surname.getText().toString().trim();
+        String userEmail = email.getText().toString().trim();;
+        String userPassword = password.getText().toString().trim();
+
+        if(username.isEmpty()){
+            name.setError("Full name is required");
+            name.requestFocus();
+            return;
+        }
+        if(userSurname.isEmpty()){
+            surname.setError("Surname is required");
+            surname.requestFocus();
+            return;
+        }
+        if(userEmail.isEmpty()){
+            email.setError("Email is required");
+            email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+            email.setError("Please provide a valid email!");
+            email.requestFocus();
+            return;
+        }
+        if(userPassword.isEmpty()){
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+        if(userPassword.length() < 6){
+            password.setError("Min password length should be 6 characters ");
+            password.requestFocus();
+            return;
+        }
+
+        createAccount(username, userSurname, userEmail, userPassword);
 
     }
 
-    public void setUpListeners()
+    private void createAccount(String username, String userSurname, String userEmail, String userPassword)
     {
+        mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(username, userSurname, userEmail);
 
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(MainActivity.this, "User has been successfully registered", Toast.LENGTH_LONG).show();
+                                            }else{
+                                                Toast.makeText(MainActivity.this, "Failed to register! Try again", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                        }else{
+                            Toast.makeText(MainActivity.this, "Failed to register", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
+/*
     private void createAccount(String email, String password)
     {
+
+
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
                             FirebaseUser user = mAuth.getCurrentUser();
 
                             firebaseDatabase = FirebaseDatabase.getInstance();
@@ -101,7 +186,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 });
     }
-
+*/
 
 
     /*
--- app/src/main/java/za/ac/iie/opsc/firebaseprac/User.java
+++ app/src/main/java/za/ac/iie/opsc/firebaseprac/User.java
@@ -1,5 +1,16 @@
 package za.ac.iie.opsc.firebaseprac;
 
 public class User {
+    public String username, userSurname, email;
 
+    //Create a constructor
+    public User(){}
+
+    //This will store the username, userSurname, and userEmail.
+    public User(String username, String userSurname, String email)
+    {
+        this.username = username;
+        this.userSurname = userSurname;
+        this.email = email;
+    }
 }
