e8936bc4ef73a98a04a4eb8647e9089d197f19c4
Darsh142001 <darshikapillay6@gmail.com>
2022-05-14 12:19:00 +0200

    deleted realtimeDatabaseConnect branch. Added the changes to master. Re-coded the createAccount for the registration in MainActivity java class. Created a User java class that stores username, userSurname, and userEmail. In the createAccount method, i then created a new user :User user = new User(username, userSurname, userEmail), which will save in the realtime database by using the FirebaseAuth.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>(){...}

--- app/src/main/java/za/ac/iie/opsc/firebaseprac/User.java
+++ app/src/main/java/za/ac/iie/opsc/firebaseprac/User.java
@@ -2,4 +2,16 @@ package za.ac.iie.opsc.firebaseprac;
 
 public class User {
 //this is where all the users details will be stored.
+public String username, userSurname, email;
+
+    //Create a constructor
+    public User(){}
+
+    //This will store the username, userSurname, and userEmail.
+    public User(String username, String userSurname, String email)
+    {
+        this.username = username;
+        this.userSurname = userSurname;
+        this.email = email;
+    }
 }
